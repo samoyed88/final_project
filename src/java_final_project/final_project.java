523e9ee72e85d6383363d04a057c2b8186556d71
samoyed88 <12130383@me.mcu.edu.tw>
2024-05-11 16:37:57 +0800

    21點: 建立一副撲克牌(四種花色,各13張)

---
+++ src/java_final_project/final_project.java
@@ -0,0 +1,31 @@
+package java_final_project;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class final_project {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//使用arrayList建立一副牌
+		ArrayList<Integer>[] poker=new ArrayList[4];
+		for(int i=0;i<4;i++) {
+			poker[i]=new ArrayList<Integer>();
+		}
+		creatpoker(poker);
+		
+
+	}
+	public static void creatpoker(ArrayList<Integer>[] poker){
+		int n=1;
+		for(int i=0;i<13;i++) {
+			poker[0].add(n);
+			poker[1].add(n);
+			poker[2].add(n);
+			poker[3].add(n);
+			if(n<10)n++;
+			else continue;
+		}
+	}
+
+}
