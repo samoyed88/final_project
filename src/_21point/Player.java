474173347cecef422c002bec0700419b568b49e8
samoyed88 <12130383@me.mcu.edu.tw>
2024-05-20 22:06:03 +0800

    21點： 將class與main分為兩個檔案

---
+++ src/_21point/Player.java
@@ -0,0 +1,69 @@
+package _21point;
+
+class player{
+	private int num,score,color,card;
+	player(){
+		this.num=0;
+		this.score=10;
+		this.color=0;
+		this.card=0;
+	}
+	//點數累加(單局)  **特例:A為1or11
+	void numadd(){
+		if(this.card>10)this.num+=10;
+		else this.num+=this.card;
+		if(this.card==1) {
+			this.num+=11;
+			if(this.num>21)this.num-=10;
+		}
+	}
+	//點數歸零
+	void numreturn() {
+		this.num=0;
+	}
+	//主程式獲取點數
+	int getnum() {
+		return this.num;
+	}
+	//分數計算(傳入莊家點數)
+	void scorecal(int n) {
+		if(this.num<=21 && this.num>n) {
+			this.score+=2;
+		}
+		else if(this.num<=21 && this.num==n) {
+			this.score+=1;
+		}
+		else if(this.num<=21 && n>21) {
+			this.score+=2;
+		}
+		else this.score-=2;
+	}
+	//主程式獲取分數
+	int getscore() {
+		return this.score;
+	}
+	//卡牌花色
+	void setcolor(int c) {
+		this.color=c;
+	}
+	//獲取花色
+	String getcolor() {
+		switch(this.color) {
+		case 0:
+			return "C";
+		case 1:
+			return "D";
+		case 2:
+			return "H";
+		case 3:
+			return "S";
+		}
+		return "Error";
+	}
+	void setcard(int c) {
+		this.card=c;
+	}
+	int getcard() {
+		return this.card;
+	}
+}
